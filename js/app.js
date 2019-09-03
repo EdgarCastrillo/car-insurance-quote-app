8bc135795ab8b3d8f68a0080f08ee88174a5a568
Edgar Castrillo <edgarcastrillo@icloud.com>
2019-09-03 16:10:14 +0200

    Read the inputs

--- js/app.js
+++ js/app.js
@@ -12,6 +12,37 @@ function Interface() {
 
 }
 
+
+//EventListeners
+const form = document.getElementById('quote-insurance')
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault()
+  console.log('presionando')
+  
+  // Read the selected brand
+  const brand = document.getElementById('brand')
+  const selectBrand = brand.options[brand.selectedIndex].value
+  console.log(selectBrand)
+  
+  // Read the year selected
+  const year = document.getElementById('year')
+  const selectYear = year.options[year.selectedIndex].value
+  console.log(selectYear)
+
+  // Read the value of radio button
+  const type = document.querySelector('input[name="type"]:checked').value
+  console.log(type)
+
+  // Create interface instance
+  const interface = new Interface()
+
+  // Check that the fields are not empty
+  
+})
+
+
+
 // Create years list
 const max = new Date().getFullYear()
 const min = max - 20
